edd7d74fd5b03130131cd0ccf532641032a425ce
Giovanni Storelli <Giovanni.storelli2001@gmail.com>
2025-08-13 23:15:20 +0000

    elimina la sfocatura dalla sfondo della startscreen

--- src/app/views/StartScreenPage.tsx
+++ src/app/views/StartScreenPage.tsx
@@ -1,8 +1,7 @@
-
-
 'use client';
 
 import React from 'react';
+import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import { Play } from 'lucide-react';
 
@@ -12,12 +11,21 @@ interface StartScreenPageProps {
 
 export const StartScreenPage = ({ onStartGame }: StartScreenPageProps) => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center p-4">
-        
+    <div className="relative min-h-screen overflow-hidden">
+      <Image
+        src="/gioca.png"
+        alt=""                // decorativo
+        fill
+        priority
+        className="object-cover scale-105 transform-gpu pointer-events-none select-none -z-10"
+      />
+
+      <div className="relative z-10 min-h-screen flex flex-col items-center justify-center p-4">
         <Button size="lg" className="text-4xl py-10 px-16" onClick={onStartGame}>
-            <Play className="mr-4 h-8 w-8" />
-            Gioca
+          <Play className="mr-4 h-8 w-8" />
+          Gioca
         </Button>
+      </div>
     </div>
   );
 };
