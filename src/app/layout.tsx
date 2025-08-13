db02061072ba161cab9904b718394d560151ecd4
Giovanni Storelli <Giovanni.storelli2001@gmail.com>
2025-08-13 23:33:58 +0000

    prova ad eliminare il global.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 
 
 import type {Metadata} from 'next';
-import '@/app/globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { cn } from '@/lib/utils';
 import { headers } from 'next/headers';
